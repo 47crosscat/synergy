9a93fd35aef426a43ef5b953bdf5805a28ba22ae
Natalia Lipgart <lipgart@gnivc.ru>
2026-03-29 21:10:21 +0300

    add factorial case

---
+++ synergy_homework/2/2_2_factorial.py
@@ -0,0 +1,39 @@
+# # без math
+
+# def factorial(n):
+#     result = 1
+#     for i in range(1, n + 1):
+#         result = result * i
+#     return result
+
+# getting_number = input("Добрый день!\nВведите, пожалуйста, число: ")
+
+# try:
+#     number = int(getting_number)
+#     if number >= 0:
+#         print(f"\n{number} -- неотрицательное число.")
+#         print(f"Факториал вашего числа равен \"{factorial(number)}\".")
+#     else:
+#         print(f"\n\"{number}\" отрицательное число. \nФакториал от отрицательного числа определить невозможно.")
+# except ValueError:
+#     print(f"\nВы ввели: \"{getting_number}\", это не число.")
+
+#с math
+
+import math
+
+print(f"\nДобрый день!")
+while True:
+    getting_number =  input("Введите, пожалуйста, неотрицательное целое число: ")
+
+    try:
+        number = int(getting_number)
+        
+        if number >= 0:
+            print(f"\n{number} -- неотрицательное число.")
+            print(f"Факториал вашего числа равен \"{math.factorial(number)}\".")
+            break
+        else:
+            print(f"\n\"{number}\" отрицательное число. \nФакториал от отрицательного числа определить невозможно.\n")
+    except ValueError:
+        print(f"\nВведённое значение \"{getting_number}\" не является неотрицательным целым числом.\n")
