4817f9e48f00068b750b69ff6cccbec86248874f
Natalia Lipgart <lipgart@gnivc.ru>
2026-03-29 23:10:22 +0300

    fix game case

--- synergy_homework/2/game/2_4_game.py
+++ synergy_homework/2/game/2_4_game.py
@@ -1,16 +1,37 @@
 import random
 
 random_number = random.randint(1,100)
+attempts = 8
 
 # print(f"Случайное число: {random_number}")
-print(f"\nДобрый день!\n\nДавайте сыграем в игру \"Угадай число\".\nЧисло выбрано случайным образом в диапазоне от 1 до 100.")
-user_number = input("\nВведите свой вариант числа: ")
+print(f"\nДобрый день!\n\nДавайте сыграем в игру \"Угадай число\".\nЧисло выбрано случайным образом в диапазоне от 1 до 100.\nЧисло попыток: {attempts}\n")
 
-check_number = int(user_number)
 
-if check_number < random_number:
-    print(f"Ваше число меньше загаданного.")
-elif check_number > random_number:
-    print(f"Ваше число больше загаданного.")
-else:
-    print(f"Вы угадали!")
+while attempts > 0:
+    user_number = input("Введите свой вариант числа: ")
+
+    try:
+        check_number = int(user_number)
+
+        if check_number >= 1 and check_number <= 100:
+
+            if check_number < random_number:
+                attempts -= 1
+                print(f"Ваше число меньше загаданного.\nКоличество оставшихся попыток: {attempts}.\n")
+            elif check_number > random_number:
+                attempts -= 1
+                print(f"Ваше число больше загаданного.\nКоличество оставшихся попыток: {attempts}.\n")
+            else:
+                print(f"Ура! Это действительно число {user_number}!")
+                break
+
+        else:
+            attempts -= 1
+            print(f"Введите число в диапазоне от 1 до 100.\nКоличество оставшихся попыток: {attempts}.\n")
+
+    except ValueError:
+        attempts -= 1
+        print(f"\nВведённое значение \"{user_number}\" не является целым числом.\nКоличество оставшихся попыток: {attempts}.\n")
+
+    if attempts == 0:
+        print(f"Игра окончена.\nИскомое число: {random_number}")
